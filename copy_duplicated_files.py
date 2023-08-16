249daeb988fa3d59f9a3271bc75895d76ce63c06
AntonioY0oung <141129553+AntonioY0oung@users.noreply.github.com>
2023-08-16 17:23:56 +0800

    Add files via upload

--- copy_duplicated_files.py
+++ copy_duplicated_files.py
@@ -1,36 +1,42 @@
 import os
 import json
 import shutil
-import argparse
 
-def find_and_replace_duplicated(old_folder, new_folder):
-   # Walk through the directory tree rooted at old_folder
-   for root, _, _ in os.walk(old_folder):
-        # Get the list of filenames in the current old folder
-        old_files = set(os.listdir(root))
-        
-        # Get the list of filenames in the new folder
-        new_files = set(os.listdir(new_folder))
-        
-        # Find duplicated filenames in both old and new folders
-        duplicated_files = old_files & new_files
-        
-        # Loop through each duplicated filename
-        for filename in duplicated_files:
-            # Create the file paths for the old and new files
-            old_file_path = os.path.join(root, filename)
-            new_file_path = os.path.join(new_folder, os.path.relpath(old_file_path, old_folder))
-            
-            # Check if the file already exists in the new folder
-            if os.path.exists(new_file_path):
-                # Overwrite the new file with the old file, preserving metadata
-                shutil.copy2(old_file_path, new_file_path)
+def find_duplicated_json_files(root_folders):
+    # Dictionary to store filenames and their corresponding file paths
+    file_dict = {}
+
+    # Traverse through the directory trees of all root folders
+    for root_folder in root_folders:
+        for folder_name, subfolders, filenames in os.walk(root_folder):
+            for filename in filenames:
+                if filename.endswith('.json'):
+                    file_path = os.path.join(folder_name, filename)
+                    if filename in file_dict:
+                        file_dict[filename].append(file_path)
+                    else:
+                        file_dict[filename] = [file_path]
+
+    # Find duplicated JSON files
+    duplicated_json_files = {filename: paths for filename, paths in file_dict.items() if len(paths) > 1}
+    
+    return duplicated_json_files
+
+def overwrite_duplicated_files(source_folder, duplicated_files):
+    for filename, paths in duplicated_files.items():
+        source_path = paths[0]  # Select the first file path as the source
+        for dest_path in paths[1:]:
+            # Overwrite the content from the source file to the destination file
+            with open(source_path, 'r') as source_file:
+                data = json.load(source_file)
+                with open(dest_path, 'w') as dest_file:
+                    json.dump(data, dest_file, indent=4)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Paths")
-    parser.add_argument('--old_file_path', help='Path to the old file')
-    parser.add_argument('--new_file_path', help='Path to the new file')
-    args = parser.parse_args()
-    old_folder = args.old_file_path
-    new_folder = args.new_file_path
-    find_and_replace_duplicated(old_folder, new_folder)
+    root_folders = ["/Users/antonioyang/Documents/Python/copy_duplicated_file/old_file_annotated", "/Users/antonioyang/Documents/Python/copy_duplicated_file/new_file_non-annotated"]
+    
+    # Find duplicated JSON files in the specified root folders
+    duplicated_json_files = find_duplicated_json_files(root_folders)
+    
+    # Overwrite duplicated JSON files in folder2 with files from folder1
+    overwrite_duplicated_files(root_folders[0], duplicated_json_files)
